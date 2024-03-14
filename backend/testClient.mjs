a78753a2dea83330bf044b9d3e7e7b18b77d7c13
Ryan <r.wang70@gmail.com>
2024-03-14 19:20:38 +0000

    change the url to onion link

--- backend/testClient.mjs
+++ backend/testClient.mjs
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 import { SocksProxyAgent } from 'socks-proxy-agent';
 
-const serverUrl = "ws://localhost:3000";
+//const serverUrl = "ws://localhost:3000";
+const serverUrl = "ws://3ugas2dxa3u6gknx7qoak7b2rroqcmolarx6k3fqivpn4x2uwgl2g3ad.onion";
 
 //create the socks proxy
 const proxyAgent = new SocksProxyAgent("socks://localhost:9050");
